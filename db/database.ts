c611ea9dbae0d32440ab1c8167c672d4d5c53465
maluda <nwitevictor7@gmail.com>
2025-02-21 16:49:37 +0100

    updated mongoose

--- db/database.ts
+++ db/database.ts
@@ -3,26 +3,26 @@ import mongoose from "mongoose";
 let isConnected = false; // Track connection state
 
 export const connectToDb = async (): Promise<void> => {
-    mongoose.set("strictQuery", true);
+  mongoose.set("strictQuery", true);
 
-    if (isConnected) {
-        console.log("MongoDB already connected");
-        return; // Return early if already connected
-    }
+  if (isConnected) {
+    console.log("MongoDB already connected");
+    return; // Return early if already connected
+  }
 
-    const MONGODB_URI = process.env.MONGODB_URI as string;
+  const MONGODB_URI = process.env.MONGODB_URI as string;
 
-    if (!MONGODB_URI) {
-        console.error("Error: MONGODB_URI is not defined.");
-        return; // Exit early if the URI is not defined
-    }
+  if (!MONGODB_URI) {
+    console.error("Error: MONGODB_URI is not defined.");
+    return; // Exit early if the URI is not defined
+  }
 
-    try {
-        await mongoose.connect(MONGODB_URI);
-        isConnected = true; // Mark as connected
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        throw error; // Rethrow the error for higher-level handling
-    }
+  try {
+    await mongoose.connect(MONGODB_URI);
+    isConnected = true; // Mark as connected
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error; // Rethrow the error for higher-level handling
+  }
 };
