f4515851f4622a8ba45a564d6ead7bbf0b0deedf
maluda <nwitevictor7@gmail.com>
2025-02-25 22:25:30 +0100

    updated gallery.tsx

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { getNFTGallery } from "@/lib/actions/NFT.actions";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { INFTInput } from "@/types";
 
 export const Gallery = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<INFTInput[] | null>(null);
+
   const sampleData = [
     {
       _id: 1,
@@ -28,22 +31,32 @@ export const Gallery = () => {
 
   const fetchData = async () => {
     try {
+      if (!window.ethereum) {
+        console.error("Metamask not installed");
+        return;
+      }
+
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
       const userWallet = accounts[0];
-      const response = await fetch(
-        `https://cytric-backend-z54j.onrender.com/nft/gallery/${userWallet}`
-      );
-      const data = await response.json();
-      setData(data);
+      const res = await getNFTGallery(userWallet);
+
+      if (res.success) {
+        setData(res.nfts ?? []);
+      }
     } catch (error) {
-      console.error(error);
+      console.error("Error fetching NFT gallery:", error);
     }
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="flex flex-col gap-8 px-4">
-      <h2 className="text-white font-bold text-xl">Your NFT Gallery</h2>
+      <h2 className="text-white font-bold text-xl">Top NFT in the market</h2>
       <div className="flex flex-col gap-6 sm:grid sm:grid-cols-2 lg:grid-cols-3">
         {sampleData.map((data) => (
           <div
@@ -65,6 +78,33 @@ export const Gallery = () => {
             </div>
           </div>
         ))}
+      </div>
+      <h2 className="text-white font-bold text-xl">Your NFT Gallery</h2>
+      <div className="flex flex-col gap-6 sm:grid sm:grid-cols-2 lg:grid-cols-3">
+        {!data || data.length === 0 ? (
+          <p className="text-white">No NFTs found for this wallet address</p>
+        ) : (
+          data.map((data) => (
+            <div
+              key={data.nftId}
+              className="flex flex-col bg-[#11182780] rounded-b-lg"
+            >
+              <Image
+                src={data.nftLogo}
+                alt={data.nftName}
+                width={400}
+                height={400}
+                className="rounded-t-lg"
+              />
+              <div className="flex flex-col gap-2 p-3">
+                <h3 className="font-bold text-[16px] text-[#FFFFFF]">
+                  {data.nftName}
+                </h3>
+                <p className="text-sm text-[#9CA3AF]">{data.nftDescription}</p>
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
